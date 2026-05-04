f87fb01c0d56d6876a84e4a3506522d8215df995
Barboza45637 <arthurvencedor2009@gmail.com>
2026-05-04 20:05:20 -0300

    primeiro commit

---
+++ src/test/java/controle_de_gastos02/ControleDeGastos02ApplicationTests.java
@@ -0,0 +1,13 @@
+package controle_de_gastos02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeGastos02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
